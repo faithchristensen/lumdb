92946f8eef20f3b8bcf1fcbbc4249a932de9e0a8
Faith Christensen <faithchristensen111@gmail.com>
2019-02-19 15:20:53 -0800

    Created a the start of the movie data base react app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = "Welcome to React";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Welcome />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -22,6 +25,14 @@ class App extends Component {
         </header>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    return (
+      <h1 className='App-title'>{welcome}</h1>
+    )
   }
 }
 
